7678f9ec9b6bbad193e6b35d5bb60d21ed4be58d
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2023-07-26 11:37:14 +0300

    updated README.md; refactor: indents in html, refactoring js and others

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-let burger = document.querySelector('.burger');
-let menu = document.querySelector('.header__nav');
-let menuLinks = menu.querySelectorAll('.nav__link');
+burgerMenu = () => {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const menuLinks = menu.querySelectorAll('.nav__link');
 
-
-burger.addEventListener('click',
-function () {
+  burger.addEventListener('click', function () {
     burger.classList.toggle('burger--active');
     menu.classList.toggle('header__nav--active');
     document.body.classList.toggle('stop-scroll');
   });
 
-menuLinks.forEach(function (el) {
-  el.addEventListener('click', function () {
-    burger.classList.remove('burger--active');
-    menu.classList.remove('header__nav--active');
-    document.body.classList.remove('stop-scroll');
+  menuLinks.forEach(function (el) {
+    el.addEventListener('click', function () {
+      burger.classList.remove('burger--active');
+      menu.classList.remove('header__nav--active');
+      document.body.classList.remove('stop-scroll');
+    });
   });
-});
+};
 
-const btnMore = document.querySelector('.articles-more');
-const articlesItems = document.querySelectorAll('.articles__item');
+const showMore = () => {
+  const btnMore = document.querySelector('.articles-more');
+  const articlesItems = document.querySelectorAll('.articles__item');
 
-btnMore.addEventListener('click', () => {
-  articlesItems.forEach(el => {
-    el.classList.add('articles__item--visible');
+  btnMore.addEventListener('click', () => {
+    articlesItems.forEach((el) => {
+      el.classList.add('articles__item--visible');
+    });
+
+    btnMore
+      .closest('.articles-center')
+      .classList.add('articles-center--hidden');
   });
+};
 
-  btnMore.closest('.articles-center').classList.add('articles-center--hidden');
-
-});
+burgerMenu();
+showMore();
